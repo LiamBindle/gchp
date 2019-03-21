4285a4de4122db99608cac86a04423736e8e5d19
Liam Bindle <liam.bindle@gmail.com>
2019-03-21 18:10:23 -0300

    changed Input_Opt member references to existing members; see #6

--- Chem_GridCompMod.F90
+++ Chem_GridCompMod.F90
@@ -861,16 +861,28 @@ CONTAINS
     ! ewl note: might be able to remove some of these...
 
     ! Max # of diagnostics
-    CALL ESMF_ConfigGetAttribute( GeosCF, Input_Opt%MAX_BPCH_DIAG,  &
+	! [lrb 2019-03-21: Is this supposed to be Input_Opt%MAX_DIAG?
+    ! CALL ESMF_ConfigGetAttribute( GeosCF, Input_Opt%MAX_BPCH_DIAG,  &
+    !                               Default = 80,                     &
+    !                               Label   = "MAX_DIAG:",            &
+    !                               __RC__                           ) 
+    CALL ESMF_ConfigGetAttribute( GeosCF, Input_Opt%MAX_DIAG,       &
                                   Default = 80,                     &
                                   Label   = "MAX_DIAG:",            &
-                                  __RC__                           ) 
+                                  __RC__                           )
+	! ]
 
     ! Max # of families per ND65 family tracer (not used for ESMF)
-    CALL ESMF_ConfigGetAttribute( GeosCF, Input_Opt%MAX_Families,   &
+	! [lrb 2019-03-21: Is this supposed to be Input_Opt%MAX_FAM?
+    ! CALL ESMF_ConfigGetAttribute( GeosCF, Input_Opt%MAX_Families,   &
+    !                               Default = 20,                     &
+    !                               Label   = "MAX_FAM:",             & 
+    !                               __RC__                           )
+    CALL ESMF_ConfigGetAttribute( GeosCF, Input_Opt%MAX_FAM,        &
                                   Default = 20,                     &
                                   Label   = "MAX_FAM:",             & 
                                   __RC__                           )
+	! ]
 
     ! # of levels in LINOZ climatology
     CALL ESMF_ConfigGetAttribute( GeosCF, Input_Opt%LINOZ_NLEVELS,  &
